c6b29593f476709ab41010b9b2ad3e65b961d8c3
@Alicia-Hatta <simthandilehulana@icloud.com>
2026-07-19 23:05:57 +0200

    modified:   static/style.css modified:   app.py

--- app.py
+++ app.py
@@ -16,3 +16,6 @@ def contact():
     return render_template('contact.html')
 if __name__ == '__main__':
     app.run(debug=True)
+    @app.route('/about')
+    def about():
+        return render_template('about.html')
